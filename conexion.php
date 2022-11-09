fcbd0ad579b41ddae04649046bb28646a2efcf67
Hector Antonio Cruz Zazueta <hcruz7@hotmail.con>
2022-11-09 00:12:20 -0700

    connections SSL

--- conexion.php
+++ conexion.php
@@ -17,12 +17,13 @@ function connect() {
     $basededatos = 'simeindb';
     $user = 'root';
     $password = 'kx3QtnkJ2Boz1zPiFc4CCEV2QheQ2oYq';
-    'opts' => array(
+    $options = array(
       PDO::MYSQL_ATTR_SSL_CA => 'isrgrootx1.pem',
-      PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+      PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+     );
 
 
-  return new PDO('mysql:host='.$host.';dbname='.$basededatos, $user, $password, $db_info['opts'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+  return new PDO('mysql:host='.$host.';dbname='.$basededatos, $user, $password, $options , array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 }
 
 ?>
