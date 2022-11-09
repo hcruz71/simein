be520a9bb37ba61690ac3cb672957dff18eee356
Hector Antonio Cruz Zazueta <hcruz7@hotmail.con>
2022-11-09 00:28:05 -0700

    connections SSL

--- componentes/nav.php
+++ componentes/nav.php
@@ -61,7 +61,7 @@
                     <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #fff">
                     
                     <?php    
-                        include 'conexion.php';
+                        //include 'conexion.php';
                         $pdo = connect();
                         $sql= "SELECT id_doctor, atencion, nombre FROM doctor where id_doctor=".$id_doctor;
                         $query = $pdo->prepare($sql);
