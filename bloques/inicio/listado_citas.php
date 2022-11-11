2ee350a795ef67440b4bc3799b1cf3574285d548
Hector Antonio Cruz Zazueta <hcruz7@hotmail.con>
2022-11-11 03:31:39 -0700

    Comentarios Flags

--- bloques/inicio/listado_citas.php
+++ bloques/inicio/listado_citas.php
@@ -33,10 +33,10 @@
                                             $fecha= date("y-m-d");
                                             echo($id_doctor);
                                             
-                                            $sql= "SELECT * FROM agenda AS A INNER JOIN pacientes AS P ON (A.id_doctor = P.id_doctor) AND (A.id_Paciente = P.id_Paciente) WHERE A.id_doctor=:idDoctor and A.START LIKE '%:fechaActual%' ORDER BY A.start ASC";
+                                            $sql= "SELECT * FROM agenda AS A INNER JOIN pacientes AS P ON (A.id_doctor = P.id_doctor) AND (A.id_Paciente = P.id_Paciente) WHERE A.id_doctor=1 and A.START LIKE '%:fechaActual%' ORDER BY A.start ASC";
                                          
                                             $query = $pdo->prepare($sql);
-                                            $query->bindParam(':idDoctor', $id_doctor, PDO::PARAM_INT);
+                                            //$query->bindParam(':idDoctor', $id_doctor, PDO::PARAM_INT);
                                             $query->bindParam(':fechaActual', $fecha, PDO::PARAM_STR);
                                             try{
                                               $query->execute();
