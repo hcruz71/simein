f329789a36fa94043fa3fd72ab73065c8d115ce4
Hector Antonio Cruz Zazueta <hcruz7@hotmail.con>
2022-11-22 17:17:29 -0700

    entra al paciente correcto

--- consultas_2.php
+++ consultas_2.php
@@ -5,12 +5,9 @@
    if(!isset($_SESSION)) {
     session_start();
 }
-if (!isset($_SESSION['id_pac_get'])){
-  $_SESSION['id_pac_get']=$_GET['id_Paciente'];
-}
-if (!isset($id_paciente)) {
-  $id_paciente=$_GET['id_Paciente'];
-}
+$_SESSION['id_pac_get']=$_GET['id_Paciente'];
+$id_paciente=$_GET['id_Paciente'];
+
   
 
  ?>
